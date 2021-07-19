b4cf83d580424e2cb33602680c88a24a6c155e53
Zain Parihar <zain.parihar@protonmail.com>
2021-07-19 01:35:30 -0400

    Added J1 and J2, as well as J3 in progress on main

---
+++ J1.py
@@ -0,0 +1,11 @@
+B = input()
+P = int(B) * 5 - 400
+
+print(P)
+
+if P == 100:
+    print(0)
+elif P > 100:
+    print(-1)
+else:
+    print(1)
--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-msg = "This is a file to test python"
-print(msg)
+listOfInstructions = []
+
+while True:
+
+    msg = "left"
+
+    currentNumber = input()
+    sum = int(currentNumber[0]) + int(currentNumber[1])
+    print(sum)
+
+    if currentNumber == "99999":
+        break
+    elif sum % 2 == 0:
+        msg = "right"
+
+    inputMessage = msg + " " + currentNumber[2] + currentNumber[3] + currentNumber[4]
+    listOfInstructions.append(inputMessage)
+    
+i = 0
+length = int(len(listOfInstructions))
+
+while i < length:
+    print(listOfInstructions[i])
+    i += 1
